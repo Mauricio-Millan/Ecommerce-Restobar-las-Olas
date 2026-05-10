31056eecb1973dd1968373169708224c1f615def
Mauricio Millan <mauriciomillanpariona247@gmail.com>
2026-05-10 13:19:29 -0500

    Creacion de backend y frontend

---
+++ BackendRestobarlasOlas/src/main/java/org/example/backendrestobarlasolas/BackendRestobarlasOlasApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendrestobarlasolas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRestobarlasOlasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendRestobarlasOlasApplication.class, args);
+    }
+
+}
---
+++ BackendRestobarlasOlas/src/test/java/org/example/backendrestobarlasolas/BackendRestobarlasOlasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendrestobarlasolas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRestobarlasOlasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
